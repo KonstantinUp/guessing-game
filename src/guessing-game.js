c6fc11f402f4d0fe9ce7098a5216291c148147f7
Starodubenko Konstantin <kostyast93@gmail.com>
2017-09-23 22:23:29 +0300

    Modified

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max=null;
+        this.max = null;
+    }
 
     setRange(min, max) {
-
+    this.min=min;
+    this.max= max;
+    this.middle;
     }
 
     guess() {
-
+        this.middle =Math.round((this.min+this.max)/2);
+        return this.middle;
     }
 
     lower() {
-
+         this.max = this.middle;
     }
 
     greater() {
-
+       this.min =this.middle;
     }
 }
 
